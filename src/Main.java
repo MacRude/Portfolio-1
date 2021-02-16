2c06e161af1afa179c39005978e232e77a855412
MacRude <78961065+MacRude@users.noreply.github.com>
2021-02-16 23:42:11 +0100

    Main update

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,24 @@
+interface shapes{
+    public void points(int i);
+    public void getShape();
+        }
+class triangle implements shapes{
+    public void points(int i){
+        i = 3;
+    }
+    public void getShape(){
+    points(3);
+    }
+}
+class rect implements shapes{
+    public void points(int i){
+        i = 4;
+    }
+    public void getShape(){
+
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
