35c21a50026b2e6f9cbeb0819596f796ad039255
MacRude <78961065+MacRude@users.noreply.github.com>
2021-02-18 22:57:15 +0100

    Updated main
    Created constructor for frame. Need a closing function for the frame, and program to stop when the function is instantialized.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
-import java.awt.Frame;
+import java.awt.Frame; //awt is Abstract Window Toolkit, which supports GUI
+import java.awt.Component;
+import java.util.Scanner;
 
 interface shapes{
     public void points(int i);
@@ -21,15 +23,37 @@ class rect implements shapes{
     }
 }
 
+class frame{
+    //Creating a frame for the shapes
+    //using a constructor, and instantiate a "Frame" object
+    Frame frame = new Frame();
+    int frameWidth;
+    int frameHeight;
+    String frameTitle;
+
+    public frame(int frameWidth,int frameHeight,String frameTitle) {
+        this.frameWidth = frameWidth;
+        this.frameHeight = frameHeight;
+        this.frameTitle = frameTitle = "Drawing board";
+        frame.setSize(frameWidth, frameHeight);
+        frame.setVisible(true); //built in boolean
+        frame.setTitle(frameTitle);
+    }
+
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Check the opened window, and explore some Shape-creation!");
+        Scanner scanner = new Scanner(System.in);
+        String stop = scanner.nextLine();
+        stop = stop.toUpperCase();
+        //initializes the object, frame.
+        new frame(300,300,"Drawing Board");
+        if(stop=="x"){
+            frame(setVisible)=true;
+        }
 
-        //Creating a frame for the shapes
-        Frame frame = new Frame();
-        int frameWidth = 300;
-        int frameHeight = 300;
-        frame.setSize(frameWidth,frameHeight);
-        frame.setVisible(true);
+
     }
 }
